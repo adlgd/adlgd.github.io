f4cac1cc606cf1545752243dd74ba38784bd638f
cellutic8 <cellutic8@gmail.com>
2026-03-14 10:07:58 +0900

    Create levels.js

---
+++ js/levels.js
@@ -0,0 +1,34 @@
+const levels = {
+  amethyst: {
+    name: "Amethyst",
+    creator: "iMist and more",
+    verifier: "wPopoff",
+    id: "119550490",
+    password: "Free Copy",
+    length: "1m 42s",
+    object: "147,177",
+    version: "2.207",
+    difficulty: "Extreme Demon",
+    rating: "Featured",
+    youtube: "4lfkzz1VCbA",
+    bgColor: "#120e1a",
+    song: "-Ricochet Love- by Waterflame (ID 233860)",
+    description: "An idea that started 5 years ago has now been brought to light. Venture into the depths of the cave to fulfill an ancient legend. Will you be the one to find the Amethyst crystal?"
+  },
+  thinkingspaceii: {
+    name: "Thinking Space II",
+    creator: "CairoX and more",
+    verifier: "Zoink",
+    id: "119544028",
+    password: "Free Copy",
+    length: "1m 9s",
+    object: "139,553",
+    version: "2.207",
+    difficulty: "Extreme Demon",
+    rating: "Featured",
+    youtube: "4lfkzz1VCbA",
+    bgColor: "#120e1a",
+    song: "Nuke Powder by MaelouX (ID 489111)",
+    description: "An idea that started 5 years ago has now been brought to light. Venture into the depths of the cave to fulfill an ancient legend. Will you be the one to find the Amethyst crystal?"
+  }
+};
