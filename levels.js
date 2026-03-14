4c88b1422f3a0539a423fd4b6ef81be2db5df7f5
cellutic8 <cellutic8@gmail.com>
2026-03-14 09:52:45 +0900

    Update levels.js

--- levels.js
+++ levels.js
@@ -4,21 +4,29 @@ const levels = {
     creator: "iMist and more",
     verifier: "wPopoff",
     id: "119550490",
+    password: "Free Copy",
+    length: "1m 42s",
+    object: "147,177",
     version: "2.207",
     difficulty: "Extreme Demon",
+    rating: "Featured",
     youtube: "https://www.youtube.com/embed/4lfkzz1VCbA",
     bgColor: "#120e1a",
-    font: "'Montserrat', sans-serif"
+    song: "-Ricochet Love- by Waterflame (ID 233860)"
   }
   thinkingspaceii: {
-    name: "Thinkingspaceii",
-    creator: "iMist and more",
-    verifier: "wPopoff",
-    id: "119550490",
+    name: "Thinking Space II",
+    creator: "CairoX and more",
+    verifier: "Zoink",
+    id: "119544028",
+    password: "Free Copy",
+    length: "1m 9s",
+    object: "139,553",
     version: "2.207",
     difficulty: "Extreme Demon",
+    rating: "Featured",
     youtube: "https://www.youtube.com/embed/4lfkzz1VCbA",
     bgColor: "#120e1a",
-    font: "'Montserrat', sans-serif"
+    song: "Nuke Powder by MaelouX (ID 489111)"
   }
 };
