96d22ab7eb9e4649826ec1d54343b944c44d2a26
cellutic8 <cellutic8@gmail.com>
2026-03-14 09:40:59 +0900

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,24 @@
+const levels = {
+  amethyst: {
+    name: "Amethyst",
+    creator: "iMist and more",
+    verifier: "wPopoff",
+    id: "119550490",
+    version: "2.207",
+    difficulty: "Extreme Demon",
+    youtube: "https://www.youtube.com/embed/4lfkzz1VCbA",
+    bgColor: "#120e1a",
+    font: "'Montserrat', sans-serif"
+  }
+  thinkingspaceii: {
+    name: "Thinkingspaceii",
+    creator: "iMist and more",
+    verifier: "wPopoff",
+    id: "119550490",
+    version: "2.207",
+    difficulty: "Extreme Demon",
+    youtube: "https://www.youtube.com/embed/4lfkzz1VCbA",
+    bgColor: "#120e1a",
+    font: "'Montserrat', sans-serif"
+  }
+};
