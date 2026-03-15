42df15e939fe0c35173134b2406cccc0cee4955c
cellutic8 <cellutic8@gmail.com>
2026-03-15 09:55:30 +0900

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+<script>
+
+function toggleMenu(){
+
+    const menu = document.getElementById("sideMenu");
+
+    if(menu.style.left === "0px") {
+        menu.style.left = "-250px";
+    } else {
+        menu.style.left = "0px";
+    }
+
+}
+
+</script>
