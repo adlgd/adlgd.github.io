ad3db38eb9ce0386eabfef1244dff9c7a002f332
cellutic8 <cellutic8@gmail.com>
2026-07-04 11:01:44 +0900

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,48 @@
-const filter = document.getElementById("statusFilter");
+const filterButton = document.getElementById("filterButton");
+const filterMenu = document.getElementById("filterMenu");
 
-filter.addEventListener("change", function () {
-    const mode = this.value;
-    const entries = document.querySelectorAll(".entry");
+filterButton.onclick = () => {
+    filterMenu.classList.toggle("show");
+};
 
-    entries.forEach(entry => {
-        const status = entry.querySelector(".sub-rank").dataset.status;
+const searchBox = document.getElementById("searchBox");
+const radios = document.querySelectorAll("input[name=status]");
 
-        if (mode === "all" || status === mode) {
-            entry.style.display = "flex";
-        } else {
-            entry.style.display = "none";
-        }
+function updateList(){
+
+    const search = searchBox.value.toLowerCase();
+
+    const status =
+        document.querySelector("input[name=status]:checked").value;
+
+    document.querySelectorAll(".entry").forEach(entry=>{
+
+        const level =
+            entry.querySelector(".name").textContent.toLowerCase();
+
+        const host =
+            entry.querySelector(".host").textContent.toLowerCase();
+
+        const s =
+            entry.querySelector(".sub-rank").dataset.status;
+
+        const searchMatch =
+            level.includes(search) || host.includes(search);
+
+        const statusMatch =
+            status==="all" || status===s;
+
+        entry.style.display =
+            searchMatch && statusMatch ? "flex" : "none";
+
     });
+
+}
+
+searchBox.addEventListener("input",updateList);
+
+radios.forEach(r=>{
+
+    r.addEventListener("change",updateList);
+
 });
